1c2af0849b0fb16a17b0ab514c07346743d0538c
ValeriyV <valeriy.vasipov@gmail.com>
2022-09-24 23:21:52 +0300

    homework1

---
+++ Homework1/task03.py
@@ -0,0 +1,23 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт 
+# номер четверти плоскости, в которой находится эта точка
+def input_int_not_null(text):
+    # Функция ввода целого числа
+    while(True):
+        try:
+            number = int(input(text))
+            if number == 0: raise ValueError
+            return number
+        except ValueError:
+            print("Необходимо ввести число, не равное нулю!")
+
+print("Введите координаты X ≠ 0 и Y ≠ 0")
+x = input_int_not_null("Введите X: ")
+y = input_int_not_null("Введите Y: ")
+if(x > 0 and y > 0):
+    print(f"X={x}, Y={y} -> 1")
+elif(x < 0 and y > 0):
+    print(f"X={x}, Y={y} -> 2")
+elif(x < 0 and y < 0):
+    print(f"X={x}, Y={y} -> 3")
+else:
+    print(f"X={x}, Y={y} -> 4")
---
+++ Homework1/task05.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+import math
+
+def input_int(text):
+    # Функция ввода целого числа
+    while(True):
+        try:
+            number = int(input(text))
+            return number
+        except ValueError:
+            print("Необходимо ввести число!")
+get_pointXY = lambda: [input_int("Введите X: "), input_int("Введите Y: ")] # ввод координат точки
+
+def find_range(point_a, point_b): # находит расстояние между точками
+    return round(math.sqrt((point_b[0]-point_a[0])**2 + (point_b[1]-point_a[1])**2), 2)
+
+print("Введите координаты первой точки:")
+point_a = get_pointXY()
+print("Введите координаты второй точки:")
+point_b = get_pointXY()
+print(find_range(point_a, point_b))
