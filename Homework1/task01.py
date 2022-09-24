b8b192b4ac9df8fa283ed973b245096b202db198
ValeriyV <valeriy.vasipov@gmail.com>
2022-09-24 23:49:01 +0300

    Add README

--- Homework1/task01.py
+++ Homework1/task01.py
@@ -6,7 +6,8 @@ input_string = input("Введите день недели: ")
 if input_string.isdigit():
     day_of_the_week = int(input_string)
     if day_of_the_week > 0 and day_of_the_week <= 7:
-        print(f'{day_of_the_week} -> Yes' if day_of_the_week >= 6 and day_of_the_week <= 7 else f'{day_of_the_week} -> No')
+        print(f'{day_of_the_week} -> Yes' if day_of_the_week >=
+              6 and day_of_the_week <= 7 else f'{day_of_the_week} -> No')
     else:
         print("Число должно быть в диапозоне от 1 до 7!")
 else:
--- Homework1/task02.py
+++ Homework1/task02.py
@@ -1,6 +1,10 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-is_predicate = lambda x, y, z: not(x or y or z) == (not x and not y and not z) # функция определеня истинности выражения
-xyz_bool_array = [[bool(int(i)) for i in str(bin(x))[2:].zfill(3)] for x in range(8)] # заполняем массив всеми комбинациями x, y, z
+def is_predicate(x, y, z): return not (x or y or z) == (
+    not x and not y and not z)  # функция определеня истинности выражения
+
+
+xyz_bool_array = [[bool(int(i)) for i in str(bin(x))[2:].zfill(3)]
+                  for x in range(8)]  # заполняем массив всеми комбинациями x, y, z
 
 print("Проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z")
 for xyz in xyz_bool_array:
