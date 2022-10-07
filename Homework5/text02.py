3b348b9961b2694674aa511d996d7baba6ad4c4b
Tirsh <valeriy.vasipov@gmail.com>
2022-10-08 01:00:01 +0300

    Homework5_task2

--- Homework5/text02.py
+++ Homework5/text02.py
@@ -1,4 +1,6 @@
-CANDIES = 200
+import random
+
+CANDIES = 2021
 
 
 def input_int(text, answers):
@@ -22,27 +24,38 @@ def choice_game_type():
 def play_vs_man(candies):
     answer = 28 if candies > 28 else candies
     turn = input_int("Осталось {} конфет, сделайте ход: ".format(candies), set(range(1, answer + 1)))
-    return candies - turn
+    return turn
 
 
-def play_vs_bot():
-    return
+def play_vs_bot(candies):
+    if candies <= 28:
+        return candies
+    best_answer = candies - (candies // 28) * 28 - 1
+    turn = best_answer if best_answer != 0 else random.randrange(1, 29)
+    print("Осталось {} конфет, бот сделал ход: {}".format(candies, turn))
+    return turn
+
+
+def lottery():
+    result = random.randrange(1, 3)
+    print("Проведена жеребьевка, первым ходит игрок {}".format(result))
+    return True if result == 1 else False
 
 
 def start_game(candies, first_player, second_player):
-    first_turn = True
+    first_turn = lottery()
     game_over = False
     winner = 0
     while not game_over:
         if candies > 0:
             if first_turn:
-                print("Ход первого игрока: ")
-                candies = first_player(candies)
+                print("\nХод первого игрока: ")
+                candies -= first_player(candies)
                 winner = 1 if candies == 0 else 0
                 first_turn = False
             else:
-                print("Ход второго игрока: ")
-                candies = second_player(candies)
+                print("\nХод второго игрока: ")
+                candies -= second_player(candies)
                 winner = 2 if candies == 0 else 0
                 first_turn = True
         else:
