6f4ba69ee20aacb1afe055086514d980cd7680e9
Tirsh <valeriy.vasipov@gmail.com>
2022-10-08 18:31:43 +0300

    Homework5_fixed

--- Homework5/task01.py
+++ Homework5/task01.py
@@ -5,8 +5,10 @@ def read_from_file(filename):
 
 def delete_word_in_text(text, removed_phrase):
     string_list = text.split()
-    return " ".join(list(filter(lambda x: True if x.find(removed_phrase) == -1 else False, string_list)))
+    return " ".join(list(filter(lambda x: removed_phrase not in x, string_list)))
 
 
 phrase = "abc"
-print(delete_word_in_text(read_from_file("text.txt"), phrase))
+dirty_text = read_from_file("text.txt")
+print(dirty_text)
+print(delete_word_in_text(dirty_text, phrase))
